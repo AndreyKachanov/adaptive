dce7e8f37312fa965acf632776b527f2b61628ce
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-14 23:15:33 +0300

    Adding images

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,6 @@
-// Browser detection for when you get desparate. A measure of last resort.
-
-// http://rog.ie/post/9089341529/html5boilerplatejs
-// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
-
-// Uncomment the below to use:
-// var b = document.documentElement;
-// b.setAttribute('data-useragent',  navigator.userAgent);
-// b.setAttribute('data-platform', navigator.platform);
+(function($){})(window.JQuery);
 
 
-function initPage(){
+$(document).ready(function() {
 
-	// your functions go here
-
-	console.log('page loaded');
-
-};
+});
