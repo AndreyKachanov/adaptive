3ece90d582eea618027d67c0111a87fd1e340c83
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-19 20:31:58 +0300

    Creating styles for desktops

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,18 @@
 (function($){})(window.JQuery);
 
 
-$(document).ready(function() {
-
+/* trigger when page is ready */
+$(document).ready(function (){
+	initialize();
 });
+
+
+-----------------------//AJAX load links from the top nav
+
+function initialize() {
+	//Click on nav to load external content through AJAX
+	$('#topnav a').click(function(e){
+		e.preventDefault();
+		$('#pages').load( e.target.href + ' #loadcontent'); //pages finished loading
+	}); //clicked on nav
+}
