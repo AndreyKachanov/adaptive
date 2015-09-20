127c0827bb11bd2b11173fa8c25e0f5c874efaa1
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-20 15:32:41 +0300

    Created ajax effect

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,6 +13,7 @@ function initialize() {
 		$('<div></div>').attr('id', 'spinner').appendTo('#pages'); //spinner
 		$('#pages').load( e.target.href + ' #loadcontent', function() { //stop spinner
 			fadespinner();
+			foodclicks();
 		}); //pages finished loading
 	}); //clicked on nav
 
@@ -23,7 +24,7 @@ function initialize() {
 function foodclicks() {
 	//click on foodlist items to see them larger image onscreen
 	$('.foodlist li').click(function(e){
-
+		
 		//Add the Overlay
 		$('<div></div>').attr('id', 'overlay').appendTo('body').hide().fadeIn("slow");
 
@@ -34,8 +35,27 @@ function foodclicks() {
 			}); //fadeout
 		}); //overlayclick
 
+		//Make a copy of the info area and place within the overlay
+		$(this).children('.info').clone().appendTo('#overlay');
+
+		//Add the spinner while the image loads
+		$('<div></div>').attr('id', 'spinner').appendTo('#overlay');
+
+		//Calculate the name of the high res image
+		largeimage=$(this).children('img').attr('src');
+		largeimage=largeimage.substr(0, largeimage.length-7)+'.jpg';
+
+		//Load the Image
+		$('<img>').attr('src',largeimage).attr('id', 'overlayimg').appendTo('#overlay').load(function(){
+			fadespinner();
+			
+			//Click to return
+			$('<div></div>').attr('id', 'clicktoreturn')
+			.appendTo('#overlay').hide().delay(500).fadeIn(400).delay(1500).fadeOut(400); //continue all at once.
+		});
 	}); //foodlist click
 }
+
 
 
 function fadespinner() {
