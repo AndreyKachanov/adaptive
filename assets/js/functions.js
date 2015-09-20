658a52cc295002d7e94945febceac4c9ff383478
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-20 23:48:32 +0300

    Created ipad design

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,11 +14,62 @@ function initialize() {
 		$('#pages').load( e.target.href + ' #loadcontent', function() { //stop spinner
 			fadespinner();
 			foodclicks();
+			tabclicks();
 		}); //pages finished loading
 	}); //clicked on nav
 
-	foodclicks();
+	// $(document).scroll(function() {
+	// 	scrollfix();
+	// }); 
+	
+	$(window).bind('orientationchange', function() {
+		//reset the overlay's width, height and position
+		$('#overlay').css('width', window.innerWidth+ 'px');
+		$('#overlay').css('height', window.innerHeight+ 'px');
+		$('#overlay').css('top', window.pageYOffset+ 'px'); 
 
+		centerimage();
+	});
+
+	foodclicks();
+	tabclicks();
+}
+
+// function scrollfix() {
+// 	ismobile=whichmobile();
+// 	if ((ismobile=='ipad')||(ismobile=='iphone')||(ismobile=='android 2')) {
+// 		$('footer').css('top',(window.pageYOffset + window.innerHeight-$('footer').height()) + 'px'); // scroll the footer navigation
+// 		$('#overlay').css('top', window.pageYOffset+ 'px'); // scroll the overlay so it stays centered
+// 		$('#overlay').css('height',window.innerHeight+"px"); //make the overlay the height of the device
+// 	}
+// }
+
+// function whichmobile() {
+// 	var useragentstring=navigator.userAgent.toLowerCase();
+// 	var mobilelist=new Array("iphone os 5","ipad; cpu os 5","iphone","ipad","android 2","android","blackberry","palmos");
+// 	for (var device in mobilelist) {	
+// 		if (useragentstring.indexOf(mobilelist[device])>=0) {
+// 			return mobilelist[device];
+// 			break;
+// 		}
+// 	}	
+// }
+
+function centerimage(){
+	//center the image
+	$('#overlayimg').css('top',((window.innerHeight - $('#overlayimg').outerHeight()) / 2)+'px');
+	$('#overlayimg').css('left',((window.innerWidth - $('#overlayimg').outerWidth()) / 2)+'px');
+}
+
+function tabclicks() {
+	$('#tabs li').click(function(e){
+		$('#tabs li').attr("class","");
+		$(this).attr('class', 'tapped');
+		
+		whichpage=$(this).attr('id').substr(3); //get the name of the page to turn on
+		$('#pages section').attr('class','foodlist hide'); //hide all of the lists
+		$('#pages #'+whichpage).attr('class','foodlist show');
+	});
 }
 
 function foodclicks() {
@@ -48,7 +99,7 @@ function foodclicks() {
 		//Load the Image
 		$('<img>').attr('src',largeimage).attr('id', 'overlayimg').appendTo('#overlay').load(function(){
 			fadespinner();
-			
+			centerimage();
 			//Click to return
 			$('<div></div>').attr('id', 'clicktoreturn')
 			.appendTo('#overlay').hide().delay(500).fadeIn(400).delay(1500).fadeOut(400); //continue all at once.
